f9b4ce98067700e955fe2783a053bcf17ffef8e5
Aditya Kr Sharma <adit.sharma.1136@gmail.com>
2026-02-22 23:47:54 +0530

    aditya

---
+++ JavaScript/Advanced DOM/Adv_DOM2 - Functions/index.js
@@ -0,0 +1,25 @@
+function add (num1 , num2){
+    return num1 + num2;
+}
+function multiply (num1 , num2){
+    return num1 * num2;
+}
+function subtract (num1 , num2){
+    return num1 - num2;
+}
+function divide (num1 , num2){
+    return num1 / num2;
+}
+function modulo (num1 , num2){
+    return num1 % num2;
+}
+
+function operator (num1 , num2 , operator){
+    return operator (num1 , num2);
+}
+
+add(2,3);
+multiply(5,4);
+subtract(98,34);
+divide(87,24);
+modulo(43,76);
