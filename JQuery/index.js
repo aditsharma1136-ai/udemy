e8d928a77b66c829437617a5465dfb98c891c8fb
Aditya Kr Sharma <adit.sharma.1136@gmail.com>
2026-02-24 22:34:45 +0530

    adi

---
+++ JQuery/index.js
@@ -0,0 +1,23 @@
+// $("h1").css("color", "lightpink");
+
+// console.log($("h1").attr("class"));
+
+// $("h1").click(function (){
+//     $("h1").css("color", "lightblue");
+// });
+
+// $("button").click(function(){
+//     $("h1").css("color", "lightblue");
+// });
+
+// $("body").keypress(function(event) {
+//     $("h1").html(event.key);
+// });
+
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color", "lightblue");
+// });
+
+// $("button").on("click", function(){
+//     $("h1").slideUp().slideDown().animate({opacity : 0.5});
+// });
